6d1c107b9275c3e57559f8837fbc2444ac131273
Amrit <109876062+ritzAmp@users.noreply.github.com>
2023-03-13 16:06:29 +0200

    Code updated!

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World! This is a sample Python Web App deployed to Azure App Services"
+    return "Hello World! This is a test Python Web App deployed to Azure App Services"
 
